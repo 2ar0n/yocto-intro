74f772a569a735a092e7f6b88f96913c0fe7c5f6
Aaron Lelouvier <aaron.lelouvier@protonmail.com>
2026-02-16 19:01:13 +0100

    Updated flashing script.

--- flash.sh
+++ flash.sh
@@ -6,11 +6,14 @@ if mount | grep /dev/mmcblk0; then
     sudo umount /dev/mmcblk0*
 fi
 
-cp build/tmp/deploy/images/raspberrypi3-64/rpi-test-image-raspberrypi3-64.rootfs.wic.bz2 ./
-bunzip2 rpi-test-image-raspberrypi3-64.rootfs.wic.bz2
-sudo dd if=rpi-test-image-raspberrypi3-64.rootfs.wic of=/dev/mmcblk0 bs=4M status=progress
+machine_name="raspberrypi3-64"
+image_name="core-image-full-cmdline"
+
+cp build/tmp/deploy/images/${machine_name}/${image_name}-${machine_name}.rootfs.wic.bz2 ./
+bunzip2 ${image_name}-${machine_name}.rootfs.wic.bz2
+sudo dd if=${image_name}-${machine_name}.rootfs.wic of=/dev/mmcblk0 bs=4M status=progress
 sleep 5
-rm rpi-test-image-raspberrypi3-64.rootfs.wic
+rm ${image_name}-${machine_name}.rootfs.wic
 sync
 echo "Checking partitions on /dev/mmcblk0..."
 sudo fdisk -l /dev/mmcblk0
